a6cfb4514f9520ac21a49199cdd6912e66d7ea7a
EnyinnayaDev <paschalekeoma9@gmail.com>
2026-06-22 12:30:02 +0100

    Add Doctor, Service, and Appointment models

---
+++ medibook/scheduler/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+    
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=100)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    duration_minutes = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+
+class Appointment(models.Model):
+    patient = models.ForeignKey(User, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.CharField(max_length=20,default='pending', choices=[('pending', 'Pending'), ('confirmed', 'Confirmed'), ('cancelled', 'Cancelled')])
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Appointment {self.id} - {self.patient.username} with {self.doctor.name} on {self.date} at {self.time}"
+    
