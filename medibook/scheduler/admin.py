16483fe7a1ffe2d64a5ef5853f74d2bf3b4949c0
EnyinnayaDev <paschalekeoma9@gmail.com>
2026-06-22 12:45:01 +0100

    Registered models to admin site

--- medibook/scheduler/admin.py
+++ medibook/scheduler/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Doctor, Service, Appointment
 # Register your models here.
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'specialization')
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'duration_minutes', 'price')
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'doctor', 'service', 'date', 'time', 'status')
+    list_filter = ('status', 'doctor')
